4fcae514e90b1e40311502d1cfd3de0487fb03f8
JLS97 <jls41@alu.ua.es>
2019-04-03 19:38:02 +0200

    borrado

--- resources/PixelAtlas.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.2" name="PixelAtlas" tilewidth="32" tileheight="32" tilecount="20" columns="4">
- <image source="PruebasCB/VenezuelaBros1/resources/PixelAtlas.png" width="128" height="160"/>
-</tileset>
