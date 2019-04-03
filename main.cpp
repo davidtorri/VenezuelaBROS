70767cfc23055577b70f738284ce1ab0719ce922
JLS97 <jls41@alu.ua.es>
2019-04-03 19:28:02 +0200

    Version Codeblocks

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include "Juego.h"
+
+using namespace tinyxml2;
+using namespace std;
+
+
+
+int main()
+{
+    //cout << "LLEGO1" << endl;
+    Juego* juego = Juego::getInstance();
+    juego->crearVentana();
+    juego->cargarMapa();
+    juego->cargarPlayer();
+    juego->cargarEnemy();
+    juego->cargarCamara();
+    juego->bucleJuego();
+
+    return 0;
+}
